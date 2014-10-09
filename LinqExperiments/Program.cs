8b5dafe1f2f6c8f7c6e2b7363e4785290e62e996
Vinay Gunnam <vinaygunnam@live.com>
2014-10-09 14:19:38 -0500

    Learning GROUPBY using LINQ

---
+++ LinqExperiments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Learning GROUPBY
+            using (var db = new DataEntities())
+            {
+                var totals = db.Order_Details
+                                .GroupBy(o => o.ProductID)
+                                .Select(x => new
+                                {
+                                    ProductID = x.Key,
+                                    Quantity = x.Sum(detail => detail.Quantity)
+                                }).OrderBy(x => x.ProductID).ToList();
+
+                totals.ForEach(t => Console.WriteLine("{0}\t\t\tCount = {1}", t.ProductID, t.Quantity));
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
